09d3d9705bc13c4a1c971bccb72ab032c36a7cec
Mykola Kuniev <Nickolas7711@gmail.com>
2023-05-21 14:02:35 +0300

    fixin

--- index.js
+++ index.js
@@ -13,40 +13,13 @@ rainbow.pop()
 rainbow.push('Vain');
 
 rainbowHtml = '';
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
 
 for (i = 0; i < rainbow.length; i++) {
-    element = rainbow[i];
 
-    switch (element) {
-        case 'Richard':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #f54336'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'Of':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #ff9901'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'York':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #ffec3b'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'Gave':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #8cc44a'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'Battle':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #03aaf4'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'In':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #4051b6'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-        case 'Vain':
-            rainbowHtml += `<div class='box'><div class='circle' style='background-color: #9c27b0'>
-            </div><div class='text'>${rainbow[i]}</div></div>`;
-            break;
-    }
+    rainbowHtml += `<div class='box'><div class='circle' style='background-color: ${colors[i]}'></div>
+    <div class='text'>${rainbow[i]}</div></div>`;
+
 }
 
 document.write(`<div class="wrapp">
